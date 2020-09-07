5f739b932889324ac08ef3ab3115fe557e3c4c1d
smbrannen1 <70416284+smbrannen1@users.noreply.github.com>
2020-09-07 15:12:58 -0500

    Add files via upload

---
+++ test_functions.py
@@ -0,0 +1,12 @@
+import unittest
+from main import camper_age_input
+
+class FunctionTestCase(unittest.TestCase)
+    def test_something(self):
+        self.assertEqual(False, True)
+
+def convert_to_months(x):
+    pass
+if __name__ == '__main__':
+    unittest.main()
+
