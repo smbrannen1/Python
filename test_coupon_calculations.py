ce98d1b7d31cae272bb986fef945b18036d07174
smbrannen1 <70416284+smbrannen1@users.noreply.github.com>
2020-09-20 17:37:10 -0500

    Add files via upload

---
+++ test_coupon_calculations.py
@@ -0,0 +1,16 @@
+"""
+Program: test_coupon_calculations.py
+Author: Suzanne Brannen
+Last Date Modified: 09/20/2020
+"""
+
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def calculate_price(price, cash_coupon, percent_coupon):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
